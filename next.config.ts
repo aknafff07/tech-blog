71506b45e3cecb3c6135c49246f5efa18358da69
Aknaf Saputra <saputraaknaf301@gmail.com>
2025-11-24 16:11:59 +0700

    fix: remove unused contentlayer dependency

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
-import { withContentlayer } from 'next-contentlayer';
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
@@ -10,7 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  reactStrictMode: true,
 };
 
-export default withContentlayer(nextConfig);
+export default nextConfig;
